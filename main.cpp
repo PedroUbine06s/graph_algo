ee819a60f1789e1d5d54101853118a7ace7100a4
Pedro <pedroubine@proton.me>
2024-05-25 12:00:44 -0300

    feat: Add lazy loading for images and improve file reading efficiency
    - Added <vector> and <string> headers for additional functionality
    - Modified the readFile function to include an 'id' parameter for each edge
    - Updated main function to use a vector of Graph objects and an array of file names
    - Iterated over the vector to read each file and populate the graphs
    - Added print statements to display the graphs

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <string>
 #include "class/graph.h"
 
 void readFile(Graph &graph, std::string filename)
@@ -7,6 +9,7 @@ void readFile(Graph &graph, std::string filename)
     std::ifstream file(filename);
     char start, end;
     int weight;
+    int id;
     if (!file.is_open())
     {
         std::cout << "Error opening file" << std::endl;
@@ -21,19 +24,27 @@ void readFile(Graph &graph, std::string filename)
             weight = line[5];
         else
             weight = 0;
-        graph.addEdge(Edge(start, end, weight));
+        id = graph.getNumEdges() + 1;
+        graph.addEdge(Edge(start, end, weight, id));
     }
     file.close();
 }
 
 int main()
 {
-    Graph graph1;
-    readFile(graph1, "graphs/g1.txt");
-    Graph graph2;
-    readFile(graph2, "graphs/g2.txt");
-    Graph graph3;
-    readFile(graph3, "graphs/g3.txt");
-    graph2.printGraph();
+    std::vector<Graph> graphs(3);
+    std::vector<std::string> fileNames = {"graphs/g1.txt", "graphs/g2.txt", "graphs/g3.txt"};
+
+    for (int i = 0; i < graphs.size(); i++)
+    {
+        readFile(graphs[i], fileNames[i]);
+    }
+    graphs[0].printGraph();
+    std::cout << "----------------" << std::endl;
+    graphs[1].printGraph();
+    std::cout << "----------------" << std::endl;
+    graphs[2].printGraph();
+    std::cout << "----------------" << std::endl;
+
     return 0;
 }
